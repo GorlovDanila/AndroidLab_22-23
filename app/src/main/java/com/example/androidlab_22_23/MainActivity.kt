916ab22876ee79e809a57b5360cae76252984e64
GorlovDanila <danildanil20030@gmail.com>
2022-09-26 15:05:24 +0300

    HW-1

--- app/src/main/java/com/example/androidlab_22_23/MainActivity.kt
+++ app/src/main/java/com/example/androidlab_22_23/MainActivity.kt
@@ -1,13 +1,36 @@
 package com.example.androidlab_22_23
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidlab_22_23.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Log.e("", "")
+        binding = ActivityMainBinding.inflate(layoutInflater)?.also {
+            setContentView(it.root)
+        }
+
+        binding?.btnFloatingAction?.setOnClickListener {
+            if (binding?.tvAll?.visibility == View.INVISIBLE &&
+                binding?.tvPersonal?.visibility == View.INVISIBLE &&
+                binding?.tvItis?.visibility == View.INVISIBLE &&
+                binding?.tvEmpty?.visibility == View.INVISIBLE) {
+                binding?.tvAll?.visibility = View.VISIBLE
+                binding?.tvPersonal?.visibility = View.VISIBLE
+                binding?.tvItis?.visibility = View.VISIBLE
+                binding?.tvEmpty?.visibility = View.VISIBLE
+            } else {
+                binding?.tvAll?.visibility = View.INVISIBLE
+                binding?.tvPersonal?.visibility = View.INVISIBLE
+                binding?.tvItis?.visibility = View.INVISIBLE
+                binding?.tvEmpty?.visibility = View.INVISIBLE
+            }
+        }
     }
 }
