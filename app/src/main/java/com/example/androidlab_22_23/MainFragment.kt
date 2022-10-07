35273ef60915835c82c155fa7b83d274d96699d0
GorlovDanila <danildanil20030@gmail.com>
2022-10-07 20:50:15 +0300

    Start

---
+++ app/src/main/java/com/example/androidlab_22_23/MainFragment.kt
@@ -0,0 +1,22 @@
+package com.example.androidlab_22_23
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MainFragment : Fragment() {
+    private var binding: FragmentMainBinding? = null
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
