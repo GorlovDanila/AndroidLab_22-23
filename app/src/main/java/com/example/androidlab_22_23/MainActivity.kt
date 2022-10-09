9dac3e67cc0c245169026937a6467d121632a9f0
GorlovDanila <danildanil20030@gmail.com>
2022-10-09 20:15:39 +0300

    add something

--- app/src/main/java/com/example/androidlab_22_23/MainActivity.kt
+++ app/src/main/java/com/example/androidlab_22_23/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.androidlab_22_23
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androidlab_22_23.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+        binding?.run {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, MainFragment(), "main").commit()
+        }
     }
 }
--- app/src/main/java/com/example/androidlab_22_23/MainFragment.kt
+++ app/src/main/java/com/example/androidlab_22_23/MainFragment.kt
@@ -2,9 +2,7 @@ package com.example.androidlab_22_23
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.example.androidlab_22_23.databinding.FragmentMainBinding
 
 class MainFragment : Fragment(R.layout.fragment_main) {
@@ -12,7 +10,42 @@ class MainFragment : Fragment(R.layout.fragment_main) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentMainBinding.bind(view)
-        binding?.run {  }
+        var counterFromDialog: Int? = arguments?.getInt(ARG_COUNTER_VALUE_DIALOG)
+        var counter = 0
+        if (counterFromDialog != null) {
+            counter = counterFromDialog.toInt()
+        }
+        binding?.run {
+
+            btnNextFragment.setOnClickListener {
+                parentFragmentManager.beginTransaction().setCustomAnimations(
+                    com.google.android.material.R.anim.abc_fade_in,
+                    com.google.android.material.R.anim.abc_fade_out,
+                    com.google.android.material.R.anim.abc_fade_in,
+                    com.google.android.material.R.anim.abc_fade_out
+                ).replace(R.id.fragment_container, SecondFragment.newInstance(counter))
+                    .addToBackStack("ToMainFragment").commit()
+            }
+            btnCounterPlusOne.setOnClickListener {
+                counter++;
+                tvCounterValue.text = "Counter value: $counter"
+            }
+            btnDialog.setOnClickListener {
+                MyDialog().show(parentFragmentManager, "TAG")
+
+            }
+        }
+    }
+
+    companion object {
+
+        private const val ARG_COUNTER_VALUE_DIALOG = "arg_counter_value"
+
+        fun newInstanceDialog(counterDialog: Int) = MainFragment().apply {
+            arguments = Bundle().apply {
+                putInt(ARG_COUNTER_VALUE_DIALOG, counterDialog)
+            }
+        }
     }
 
     override fun onDestroyView() {
