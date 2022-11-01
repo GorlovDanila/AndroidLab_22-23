5a81e92bd408e6fd13ee5cc9adb1decad279e9fa
GorlovDanila <danildanil20030@gmail.com>
2022-11-01 09:03:24 +0300

    add graph and controller

--- app/src/main/java/com/example/androidlab_22_23/MainActivity.kt
+++ app/src/main/java/com/example/androidlab_22_23/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.androidlab_22_23
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+//    private var binding: Act
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    val controller =
+        (supportFragmentManager.findFragmentById(R.id.main_fragment) as NavHostFragment)
+            .navController
     }
 }
