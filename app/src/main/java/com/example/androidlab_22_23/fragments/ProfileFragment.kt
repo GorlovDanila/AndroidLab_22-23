20ce18f8338bc6f15e0ba314e2160563ae2f4c07
GorlovDanila <danildanil20030@gmail.com>
2022-11-02 17:07:30 +0300

    Complete!!!

--- app/src/main/java/com/example/androidlab_22_23/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/androidlab_22_23/fragments/ProfileFragment.kt
@@ -3,5 +3,5 @@ package com.example.androidlab_22_23.fragments
 import androidx.fragment.app.Fragment
 import com.example.androidlab_22_23.R
 
-class MainFragment : Fragment(R.layout.fragment_main) {
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
 }
